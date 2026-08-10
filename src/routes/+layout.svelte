e555c02f8e13193b81854fe7971313e301ba3b50
Flo Kayser <flokayser.fk@gmail.com>
2026-08-10 17:57:12 +0200

    feat: add basic css

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,12 @@
-<script lang="ts">
-	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+<script>
+	import '$lib/Css/app.css';
+    import GlobalNavigation from '$lib/components/global/globalNavigation.svelte';
 
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head><link rel="icon" href='/favicon.svg' /></svelte:head>
+
+<GlobalNavigation />
 {@render children()}
+
